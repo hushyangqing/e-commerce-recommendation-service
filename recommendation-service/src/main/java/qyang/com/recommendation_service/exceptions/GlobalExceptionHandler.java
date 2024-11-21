0669210c042ff2a37c137d621b9910986499e61d
hushyangqing <hushyangqing@gmail.com>
2024-11-20 16:42:29 -0800

    exceptions

---
+++ recommendation-service/src/main/java/qyang/com/recommendation_service/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package qyang.com.recommendation_service.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import qyang.com.recommendation_service.dtos.ErrorResponse;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleUserAlreadyExists(UserAlreadyExistsException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception e) {
+        return ResponseEntity
+                .internalServerError()
+                .body(new ErrorResponse("An unexpected error occurred"));
+    }
+}
